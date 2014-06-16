87df7e020235ab190bcb4906b3af51c47e098e72
SuperJMN <superjmn@outlook.com>
2014-06-16 12:37:25 +0200

    Attempt to solve method chained warning when using Fluent Interfaces

--- CleanCode/src/CleanCode/Features/ExtensionMethods.cs
+++ CleanCode/src/CleanCode/Features/ExtensionMethods.cs
@@ -5,6 +5,8 @@ using JetBrains.ReSharper.Psi.Tree;
 
 namespace CleanCode.Features
 {
+    using System.Collections.Generic;
+
     public static class ExtensionMethods
     {
         public static int CountChildren<T>(this ITreeNode node) where T : ITreeNode
@@ -19,6 +21,26 @@ namespace CleanCode.Features
                 count += CountChildren<T>(childNodes);
             }
             return count;
+        }
+
+
+
+        public static IEnumerable<T> GetChildrenRecursive<T>(this ITreeNode node) where T : ITreeNode
+        {
+            var nodeChildren = node.Children().ToList();
+
+            var list = new List<T>();
+
+            var childOfType = nodeChildren.OfType<T>();
+            list.AddRange(childOfType);
+
+            foreach (var childNode in nodeChildren)
+            {
+                var childrenOfType = GetChildrenRecursive<T>(childNode);
+                list.AddRange(childrenOfType);
+            }
+
+            return list;
         }
 
         
--- CleanCode/src/CleanCode/Features/TooManyChainedReferences/TooManyChainedReferencesCheck.cs
+++ CleanCode/src/CleanCode/Features/TooManyChainedReferences/TooManyChainedReferencesCheck.cs
@@ -1,4 +1,3 @@
-using CleanCode.Features.ChainedReferences;
 using CleanCode.Resources;
 using CleanCode.Settings;
 using JetBrains.Application.Settings;
@@ -6,9 +5,16 @@ using JetBrains.ReSharper.Daemon.CSharp.Stages;
 using JetBrains.ReSharper.Daemon.Stages;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
+using System.Collections.Generic;
+using System.Linq;
+using CleanCode.Features.ChainedReferences;
+using JetBrains.ReSharper.Psi;
+
 
 namespace CleanCode.Features.TooManyChainedReferences
 {
+    using JetBrains.DocumentModel;
+
     public class TooManyChainedReferencesCheck : SimpleCheck<IReferenceExpression, int>
     {
         public TooManyChainedReferencesCheck(IContextBoundSettingsStore settingsStore)
@@ -16,30 +22,76 @@ namespace CleanCode.Features.TooManyChainedReferences
         {
         }
 
-        protected override void ExecuteCore(IReferenceExpression methodDeclaration, IHighlightingConsumer consumer)
+        protected override void ExecuteCore(IReferenceExpression referenceExpression, IHighlightingConsumer consumer)
         {
-            if (methodDeclaration != null && !ParentIsReference(methodDeclaration))
+            if (referenceExpression != null)
             {
-                ProcessReference(methodDeclaration, consumer);
+                ProcessReference(referenceExpression, consumer);
             }
         }
 
         private void ProcessReference(IReferenceExpression reference, IHighlightingConsumer consumer)
         {
-            var length = reference.CountChildren<IReferenceExpression>();
-            var maximumChainedReferences = Threshold;
+            
+            var count = reference.CountChildren<IReferenceExpression>();
 
-            if (length > maximumChainedReferences)
+            if (count >= Threshold)
             {
-                var highlighting = new Highlighting(Warnings.ChainedReferences);
-                consumer.AddHighlighting(highlighting, reference.GetDocumentRange());
+                var rootType = GetReturnTypeFrom(reference.Reference);
+
+                if (IsStillUnknown(rootType))
+                {
+                    return;
+                }
+
+                var childrenTypes = this.GetTypesFromChildren(reference);
+
+                if (!EveryTypeIsTheSame(rootType, childrenTypes))
+                {
+                    AddHighlightning(reference, consumer);
+                }              
             }
         }
 
-        private static bool ParentIsReference(ITreeNode element)
+        private IEnumerable<IType> GetTypesFromChildren(IReferenceExpression reference)
         {
-            var reference = element.Parent as IReferenceExpression;
-            return reference != null;
+            var children = reference.GetChildrenRecursive<IReferenceExpression>();
+
+            var typesFromChildren = children.Select(expression => this.GetReturnTypeFrom(expression.Reference));
+            return typesFromChildren;
+        }
+
+        private static void AddHighlightning(IReferenceExpression reference, IHighlightingConsumer consumer)
+        {
+            var highlighting = new Highlighting(Warnings.ChainedReferences);
+            var nameIdentifier = reference.NameIdentifier;
+            consumer.AddHighlighting(highlighting, nameIdentifier.GetDocumentRange());
+        }
+
+        private static bool EveryTypeIsTheSame(IType type, IEnumerable<IType> typesFromChildren)
+        {
+            return typesFromChildren.Any(otherType => Equals(type, otherType));
+        }
+
+        private static bool IsStillUnknown(IType type)
+        {
+            return type == null;
+        }
+
+        private IType GetReturnTypeFrom(IReferenceExpressionReference reference)
+        {
+            if (reference.CurrentResolveResult != null)
+            {
+                var declaredElement = reference.CurrentResolveResult.DeclaredElement;
+                var parameternsOwner = declaredElement as IParametersOwner;
+                if (parameternsOwner != null)
+                {
+                    var r = parameternsOwner.ReturnType;
+                    return r;
+                }                
+            }
+
+            return null;
         }
 
         protected override int Threshold
