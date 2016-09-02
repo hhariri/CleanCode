2cdc312fc187920bcc1f24ddcd54b94caf48320f
Matt Ellis <m.t.ellis@gmail.com>
2016-09-02 17:34:40 +0100

    Updating nuspec

--- CleanCode/src/CleanCode/Properties/AssemblyInfo.cs
+++ CleanCode/src/CleanCode/Properties/AssemblyInfo.cs
@@ -31,15 +31,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Hadi Hariri")]
 [assembly: AssemblyProduct("ReSharper Clean Code Plugin")]
 [assembly: AssemblyDescription("Automates some of the concepts in Uncle Bob's Clean Code book")]
-[assembly: AssemblyCopyright("Copyright ® 2014 Hadi Hariri and Contributors")]
+[assembly: AssemblyCopyright("Copyright ® 2016 Hadi Hariri and Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("97927FF9-8C9C-4DC5-A309-29C23F41DA47")]
 
-[assembly: AssemblyVersion("4.0.0.*")]
-
-//[assembly: PluginTitle("Clean Code")]
-//[assembly: PluginDescription("Automates some of the concepts in Uncle Bob's Clean Code book")]
-//[assembly: PluginVendor("Hadi Hariri and Contributors")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
