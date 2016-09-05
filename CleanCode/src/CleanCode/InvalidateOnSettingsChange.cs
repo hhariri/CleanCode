0a6d2d31f6e7e4c3063b690084755a2c94e672c3
Matt Ellis <m.t.ellis@gmail.com>
2016-09-05 17:09:14 +0100

    Replace multiple settings watchers with just one

---
+++ CleanCode/src/CleanCode/InvalidateOnSettingsChange.cs
@@ -0,0 +1,18 @@
+using CleanCode.Settings;
+using JetBrains.Application.Settings;
+using JetBrains.DataFlow;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+
+namespace CleanCode
+{
+    [SolutionComponent]
+    public class InvalidateOnSettingsChange
+    {
+        public InvalidateOnSettingsChange(Lifetime lifetime, IDaemon daemon, ISettingsStore settingsStore)
+        {
+            var settingsKey = settingsStore.Schema.GetKey<CleanCodeSettings>();
+            settingsStore.AdviseChange(lifetime, settingsKey, daemon.Invalidate);
+        }
+    }
+}
