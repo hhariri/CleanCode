ca8b310f355abb244fd9afe0651e6f87ff8376e1
Wolfgang Janz <wolfgang.janz@exxcellent.de>
2016-09-29 20:10:11 +0200

    feature(): Add MethodNameNotMeaningful inspection to vb.net

---
+++ CleanCode/src/CleanCode/Features/MethodNameNotMeaningful/MethodNameNotMeaningfulCheckVb.cs
@@ -0,0 +1,33 @@
+using CleanCode.Settings;
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.VB.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace CleanCode.Features.MethodNameNotMeaningful
+{
+    [ElementProblemAnalyzer(typeof(IMethodDeclaration), HighlightingTypes = new []
+    {
+        typeof(MethodNameNotMeaningfulHighlighting)
+    })]
+    public class MethodNameNotMeaningfulCheckVb : ElementProblemAnalyzer<IMethodDeclaration>
+    {
+        protected override void Run(IMethodDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            if (element.Name == null)
+                return;
+
+            var minimumMethodNameLength = data.SettingsStore.GetValue((CleanCodeSettings s) => s.MinimumMeaningfulMethodNameLength);
+
+            var name = element.Name.GetText();
+            var methodNameLength = name.Length;
+            if (methodNameLength < minimumMethodNameLength)
+            {
+                var documentRange = element.GetNameDocumentRange();
+                var highlighting = new MethodNameNotMeaningfulHighlighting(documentRange);
+                consumer.AddHighlighting(highlighting);
+            }
+        }
+    }
+}
